ee90cbee1b9c1ce93152d52bfaeaaa85ff883c8d
Stephanie <smccumsey@gmail.com>
2017-12-21 21:52:26 -0800

    restart

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var path = require('path');
-var hike = require('./routes/hike');
-
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -11,9 +9,6 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
-app.get('/hikes', hike.index);
-app.post('/add_hike', hike.add_hike);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
