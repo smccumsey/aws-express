6ca7b98a7ca6397c9c327226df27ed1eb2c2f104
Stephanie McCumsey <smccumsey@gmail.com>
2018-01-12 18:42:31 -0800

    fixed eug route

--- routes/eugene.js
+++ routes/eugene.js
@@ -1,9 +1,3 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET eugene project. */
-router.get('/eugene', function(req, res, next) {
-  res.render('eugene', { title: 'Eugene Project' });
-});
-
-module.exports = router;
+exports.index = function(req, res) {
+ res.render('eugene', {title: 'Eugene Project'});
+};
